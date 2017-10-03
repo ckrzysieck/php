811d56a47f914c66382ba0d1cf53bec82e5fc28d
ckrzysieck <ckrzysieck@gmail.com>
2017-10-03 22:56:16 +0200

    Szyfr Cezara - nieskończony

---
+++ szyfr_cezara.php
@@ -0,0 +1,34 @@
+<?php
+
+// Każdą literę tekstu jawnego zamieniamy na literę przesuniętą o 3 miejsca w prawo. I tak literę A szyfrujemy jako literę D, literę B jako E itd. W przypadku litery Z wybieramy literę C. W celu odszyfrowania tekstu powtarzamy operację tym razem przesuwając litery o 3 pozycje w lewo - http://pl.spoj.com/problems/JSZYCER/
+
+//$szyfr = 'A';
+
+// if ('A') {
+// 	echo 'D';
+// }
+// if ('B') {
+// 	echo 'E';
+// }
+// if ('C') {
+// 	echo 'F';
+// }
+
+$tablica = range('A', 'Z', 1);
+
+
+
+
+
+
+
+
+
+for ($i=0; $i < 3; $i++) { 
+	
+}
+
+
+foreach ($tablica as $key => $value) {
+	# code...
+}
