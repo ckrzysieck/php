379595ca7f2e5d812f764e95c3012990e4138229
ckrzysieck <ckrzysieck@gmail.com>
2017-11-16 22:02:40 +0100

    Porządki - Funkcje

--- lib/figury.php
+++ lib/figury.php
@@ -1,1 +1,16 @@
 <?php
+
+// Generowanie figury (długość x szerokość)
+
+// $dlugosc = $argv[1];
+// $szerokosc = $argv[2];
+// $znak = $argv[3];
+
+function rysowanie_figury()
+for ($i=0; $i < $dlugosc; $i++) { 
+	for ($j=1; $j < $szerokosc; $j++) {
+		echo "$znak";
+	}
+	echo "$znak";
+	echo "\n";
+}
